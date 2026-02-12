0734ef543ba7edecb82ef8126b5051c29d8212ba
ciberman2002-lab <ciberman2.00.2@gmail.com>
2026-02-12 01:40:47 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from "@astrojs/vercel/serverless"; // ← Recomendado para Vercel
 import markdoc from "@astrojs/markdoc";
 
 export default defineConfig({
-  output: "server",
+  output: "static",
 
   adapter: vercel(),
 
