68d0a04bbdf64f0b8729a2b09df4e155d5f5fa66
ciberman2002-lab <ciberman2.00.2@gmail.com>
2026-02-27 18:04:51 -0300

    Update astro.config.mjs partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import keystatic from "@keystatic/astro";
 import vercel from "@astrojs/vercel/serverless"; // ← Recomendado para Vercel
 import markdoc from "@astrojs/markdoc";
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   output: "static",
@@ -12,7 +13,11 @@ export default defineConfig({
   trailingSlash: 'never',
   adapter: vercel(),
 
-  integrations: [react(), keystatic(), markdoc(), sitemap()],
+  integrations: [react(), keystatic(), markdoc(), sitemap(), partytown({
+      config: { 
+        forward: ["dataLayer.push"],
+      },
+    }),],
 
   vite: {
     plugins: [tailwind()],
